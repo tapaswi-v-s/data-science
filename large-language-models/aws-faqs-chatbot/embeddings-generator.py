463af99c627a0ec94a1bb61003c9bd6b53c2c569
Tapaswi <satyapanthi.t@northeastern.edu>
2024-07-27 18:02:49 -0400

    updated embeddings-generator.py

--- large-language-models/aws-faqs-chatbot/embeddings-generator.py
+++ large-language-models/aws-faqs-chatbot/embeddings-generator.py
@@ -1,30 +1,27 @@
-from langchain_community.document_loaders.csv_loader import CSVLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+import pandas as pd
+from langchain.docstore.document import Document as LangchainDocument
 from langchain_huggingface import HuggingFaceEmbeddings
+from tqdm.auto import tqdm
 from langchain_community.vectorstores import Chroma
 import os, warnings
-import streamlit as st
+from dotenv import load_dotenv
 warnings.filterwarnings('ignore')
+load_dotenv()
 
-loader = CSVLoader(file_path="aws_faqs.csv", encoding='utf-8')
-documents = loader.load()
+df = pd.read_csv('aws_faqs.csv')
+df = df.dropna()
 
-# Split the text
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-texts = text_splitter.split_documents(documents)
+langchain_docs = [LangchainDocument(page_content=row[1]["answer"], 
+                                    metadata={"faq": row[1]["question"]}) 
+                  for row in tqdm(df.iterrows())]
 
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = st.secrets["huggingface_api_token"] # Don't forget to add your hugging face token
-embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+# os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+embedding_model = HuggingFaceEmbeddings(
+        model_name='sentence-transformers/all-MiniLM-L6-v2',
+        encode_kwargs={"normalize_embeddings": True}  # set True to compute cosine similarity
+)
 
-# Convert texts to embeddings
-try:
-  embeddings = embedding_model.embed_documents([doc.page_content for doc in texts])
-except Exception as e:
-  print(f"Error creating embeddings: {e}")
-
-    # Initialize Chroma vector store
-vector_store = Chroma(embedding_function=embedding_model, persist_directory="data")
-# Add documents to the vector store
-vector_store.add_documents(documents=texts)
+vector_store = Chroma(embedding_function=embedding_model, persist_directory="embeddings")
+vector_store.add_documents(documents=langchain_docs)
 
 print("Embeddings generated and stored successfully.")
